ca25e8f3a8f75b9e6eb1654364fd1ba4c6fe5677
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-15 23:08:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 require 'pry'
+=======
+>>>>>>> 597a8f98fe7bf48f10681c2f93b196651e20809c
 class Artist
   attr_accessor :name
   
@@ -36,6 +39,7 @@ class Artist
   def self.find(name)
     @@all.find {|artist| artist.name == name}
   end
+<<<<<<< HEAD
 
   def print_songs
     self.songs.collect do |song|
@@ -43,4 +47,11 @@ class Artist
     end
   end
 
+=======
+  
+  def print_songs
+    self.songs.each {|song| song.name}
+  end
+  
+>>>>>>> 597a8f98fe7bf48f10681c2f93b196651e20809c
 end
